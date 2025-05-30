f9cd5d6b8032cde2a4c3be088e89e61236ac8e82
Rohit Mahajan <rohitmahajan6942089@gamil.com>
2025-05-30 22:14:51 +0530

    file pushed

---
+++ Script.js
@@ -0,0 +1,7 @@
+
+$("h1").css("color", "red");
+$("h1").css("font-size", "5rem");
+
+$(document).keypress(function(event){
+    $("h1").text(event.key);
+})
---
+++ index.js
@@ -0,0 +1,6 @@
+var numberOfDrumButtons=document.querySelectorAll(".drum").length;for(var i=0;i<numberOfDrumButtons;i++){document.querySelectorAll(".drum")[i].addEventListener("click",function(){var buttonInnerHTML=this.innerHTML;console.log(buttonInnerHTML);makeSound(buttonInnerHTML)
+  addAnimation(buttonInnerHTML)})}
+  document.addEventListener("keypress",function(event){console.log(event.key);makeSound(event.key);addAnimation(event.key)});function makeSound(key){switch(key){case "w":var tom1=new Audio("sounds/tom-1.mp3");tom1.play();break;case "a":var tom1=new Audio("sounds/tom-2.mp3");tom1.play();break;case "s":var tom1=new Audio("sounds/tom-3.mp3");tom1.play();break;case "d":var tom1=new Audio("sounds/tom-4.mp3");tom1.play();break;case "j":var tom1=new Audio("sounds/snare.mp3");tom1.play();break;case "k":var tom1=new Audio("sounds/crash.mp3");tom1.play();break;case "l":var tom1=new Audio("sounds/kick-bass.mp3");tom1.play();break}}
+  function addAnimation(button){var currentButton=document.querySelector("."+button);if(currentButton){currentButton.classList.add("pressed");setTimeout(function(){currentButton.classList.remove("pressed")},100)}}
+
+  //minifyt the code using https://javascript-minifier.com/ or https://www.minifier.org/
